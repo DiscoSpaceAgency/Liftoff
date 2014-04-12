0315f2c274327af97e1d074437d6d37e3af87ac1
Michael Helmbrecht <me@mrh.is>
2014-04-12 14:50:08 -0400

    Add mission and event classes

---
+++ Liftoff/Data/DSAMission.h
@@ -0,0 +1,37 @@
+//
+//  DSAMission.h
+//  Liftoff
+//
+//  Created by Michael on 4/12/14.
+//  Copyright (c) 2014 Disco Space Agency. All rights reserved.
+//
+
+typedef NS_ENUM(NSUInteger, DSAAgency) {
+    DSAAgencyNASA,
+    DSAAgencyJAXA,
+    DSAAgencyESA,
+    DSAAgencyRoscosmos,
+    DSAAgencyCSA
+};
+typedef NS_ENUM(NSUInteger, DSAMissionType) {
+    kDSAMissionTypeSatellite = 1,
+    kDSAMissionTypeRover = 2,
+    kDSAMissionTypeManned = 3,
+    kDSAMissionTypeOther = 4
+};
+
+@interface DSAMission : NSObject
+
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSDate *startDate;
+@property (strong, nonatomic) NSDate *endDate;
+@property (strong, nonatomic) NSArray *agencies;
+@property (readwrite, nonatomic) DSAMissionType type;
+@property (strong, nonatomic) NSURL *websiteURL;
+@property (strong, nonatomic) NSURL *imageURL;
+@property (strong, nonatomic) NSString *quickDescription;
+@property (strong, nonatomic) NSString *longDescription;
+@property (readwrite, nonatomic) NSInteger id;
+@property (strong, nonatomic) NSArray *events;
+
+@end
