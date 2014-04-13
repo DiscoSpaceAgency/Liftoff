fe0d946a65b9c33795f9af1a071c550dbe2036bf
Michael Helmbrecht <me@mrh.is>
2014-04-13 04:52:33 -0400

    Fix for hanging text off timelines

--- Liftoff/Timeline/DSATimelineViewCell.m
+++ Liftoff/Timeline/DSATimelineViewCell.m
@@ -43,7 +43,7 @@
         _missionNameLabel.text = _mission.name;
         _agencyLabel.text = _mission.agencyString;
         [_timelineStrip setFrame:CGRectMake(10 + [DSATimelineOffsetManager sharedInstance].offset + [DSATimelineWidthCalculator position:_mission.startDate], 0, [DSATimelineWidthCalculator widthForStart:_mission.startDate end:_mission.endDate], self.frame.size.height)];
-        CGFloat labelMinX = MAX(_timelineStrip.frame.origin.x + 10, 10);
+        CGFloat labelMinX = MAX(-_timelineStrip.frame.origin.x + 10, 10);
         [_missionNameLabel setFrame:CGRectMake(labelMinX, 10, self.frame.size.width - 40, 30)];
         [_agencyLabel setFrame:CGRectMake(labelMinX, 35, self.frame.size.width - 40, 20)];
     }
@@ -54,7 +54,7 @@
 - (void)pan:(NSNotification *)notification
 {
     NSInteger offset = [(NSNumber *)notification.userInfo[@"translate"] integerValue];
-    CGFloat labelX = MAX(20 + offset + [DSATimelineWidthCalculator position:MIN(_mission.startDate, _mission.endDate)], 10);
+    CGFloat labelX = MAX(-offset - [DSATimelineWidthCalculator position:MIN(_mission.startDate,_mission.endDate)], 10);
     [UIView animateWithDuration:0.1 animations:^{
         [_timelineStrip setFrame:CGRectMake(10 + offset + [DSATimelineWidthCalculator position:_mission.startDate], 0, [DSATimelineWidthCalculator widthForStart:_mission.startDate end:_mission.endDate], self.frame.size.height)];
         [_missionNameLabel setFrame:CGRectMake(labelX, 10, self.frame.size.width - 40, 30)];
@@ -64,9 +64,9 @@
 
 - (void)resetOffset:(NSNotification *)notification
 {
-    [UIView animateWithDuration:0.2 animations:^{
+    [UIView animateWithDuration:0.1 animations:^{
         [_timelineStrip setFrame:CGRectMake(10 + [DSATimelineOffsetManager sharedInstance].offset + [DSATimelineWidthCalculator position:_mission.startDate], 0, [DSATimelineWidthCalculator widthForStart:_mission.startDate end:_mission.endDate], self.frame.size.height)];
-        CGFloat labelMinX = MAX(_timelineStrip.frame.origin.x + 10, 10);
+        CGFloat labelMinX = MAX(-_timelineStrip.frame.origin.x + 10, 10);
         [_missionNameLabel setFrame:CGRectMake(labelMinX, 10, self.frame.size.width - 40, 30)];
         [_agencyLabel setFrame:CGRectMake(labelMinX, 35, self.frame.size.width - 40, 20)];
     }];
@@ -77,7 +77,7 @@
     _mission = nil;
     _missionNameLabel.text = @"";
     [_timelineStrip setFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
-    CGFloat labelMinX = MAX(_timelineStrip.frame.origin.x + 10, 10);
+    CGFloat labelMinX = MAX(-_timelineStrip.frame.origin.x + 10, 10);
     [_missionNameLabel setFrame:CGRectMake(labelMinX, 10, self.frame.size.width - 40, 30)];
     [_agencyLabel setFrame:CGRectMake(labelMinX, 35, self.frame.size.width - 40, 20)];
     [[NSNotificationCenter defaultCenter] removeObserver:self];
